6c737d2928e37d3e4899963dccf44fe5b31bbdaf
Veerrohit Veeravdivel <veerrohitv08@gmail.com>
2021-07-02 10:54:58 -0700

    interface go brrrrr

--- src/screens/homeScreen.tsx
+++ src/screens/homeScreen.tsx
@@ -1,42 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, FlatList, ScrollView } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	TouchableOpacity,
+	FlatList,
+	ScrollView,
+} from 'react-native';
+
+interface Image {
+	title: string;
+}
 
 export default function homeScreen({ navigation }) {
-
-	const friends =[
-		{ title: 'Image #0'},
-		{ title: 'Image #1'},
-		{ title: 'Image #2'},
-		{ title: 'Image #3'},
-		{ title: 'Image #4'},
-		{ title: 'Image #5'},
-		{ title: 'Image #6'},
-		{ title: 'Image #7'},
-		{ title: 'Image #8'},
-		  
-	  ]  
+	const friends: Image[] = [
+		{ title: 'Image #0' },
+		{ title: 'Image #1' },
+		{ title: 'Image #2' },
+		{ title: 'Image #3' },
+		{ title: 'Image #4' },
+		{ title: 'Image #5' },
+		{ title: 'Image #6' },
+		{ title: 'Image #7' },
+		{ title: 'Image #8' },
+	];
 	return (
 		<SafeAreaView style={styles.container}>
 			<View style={styles.Home}>
 				<Text style={styles.HomeText}> Home</Text>
 			</View>
-			<TouchableOpacity onPress={() => navigation.navigate('Upload')} style ={styles.Upload}>
+			<TouchableOpacity onPress={() => navigation.navigate('Upload')} style={styles.Upload}>
 				<Text style={styles.UploadText}>Upload Image</Text>
 			</TouchableOpacity>
 			<FlatList
 				showsVerticalScrollIndicator={false}
 				keyExtractor={(friend) => friend.title}
-				data={friends} 
+				data={friends}
 				renderItem={({ item }) => {
-          // item === {name: 'Friend #1' ...}
-          return (
-            <Text style={styles.textStyle}>
-              {item.title}
-            </Text>
-          )
-      }}
-    />
+					// item === {name: 'Friend #1' ...}
+					return <Text style={styles.textStyle}>{item.title}</Text>;
+				}}
+			/>
 		</SafeAreaView>
 	);
 }
@@ -46,33 +52,32 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: '#fff',
 	},
-	Home:{
+	Home: {
 		marginTop: 25,
 		margin: 15,
 		height: 150,
 		backgroundColor: '#a9ffa1',
-		borderRadius: 20
+		borderRadius: 20,
 	},
-	Upload:{
+	Upload: {
 		margin: 15,
 		height: 250,
 		backgroundColor: '#3fffa1',
-		borderRadius: 25
+		borderRadius: 25,
 	},
-	UploadText:{
+	UploadText: {
 		alignSelf: 'center',
 		fontSize: 50,
 		fontWeight: 'bold',
-		color: 'white'
-
+		color: 'white',
 	},
 	HomeText: {
 		alignSelf: 'center',
 		fontWeight: 'bold',
 		fontSize: 50,
-		color: 'white'
+		color: 'white',
 	},
-	textStyle:{
-		marginVertical: 5
-	}
+	textStyle: {
+		marginVertical: 5,
+	},
 });
