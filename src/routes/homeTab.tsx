e3246a427f8b1d558894fd5a131d9e3c4245509e
Veerrohit Veeravdivel <veerrohitv08@gmail.com>
2021-07-02 13:21:57 -0700

    made the tab bar look better :)

--- src/routes/homeTab.tsx
+++ src/routes/homeTab.tsx
@@ -27,9 +27,16 @@ export default function Navigator() {
 							iconName = focused ? 'star' : 'star-outline';
 						}
 						//@ts-ignore
-						return <Ionicons name={iconName} size={size} color={color} />;
+						return <Ionicons name={iconName} size={size} color="white" />;
 					},
-				})}>
+				})}
+				tabBarOptions={{
+					style: {
+						backgroundColor: '#86d3c6',
+					},
+					activeTintColor: '#ffffff',
+					inactiveTintColor: '#ffffff',
+				}}>
 				<Tab.Screen name="Upload" component={photoScreen} />
 				<Tab.Screen name="Home" component={homeScreen} />
 				<Tab.Screen name="Details" component={reviewDetails} />
