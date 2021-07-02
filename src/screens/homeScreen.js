34b54a06100e916d6e44c0297fd2730057eb0cde
aappachi <aappachi@gmail.com>
2021-07-02 09:50:54 -0700

    Styling

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 
-export default function homeScreen() {
+export default function homeScreen({ navigation }) {
 	return (
 		<SafeAreaView style={styles.container}>
 			<View style={styles.Home}>
 
 			</View>
-			<View>
+			<TouchableOpacity onPress={() => navigation.navigate('Upload')} style ={styles.Upload}>
+				{/* <Text>Upload Image</Text> */}
+			</TouchableOpacity>
 
-			</View>
 		</SafeAreaView>
 	);
 }
@@ -26,5 +27,11 @@ const styles = StyleSheet.create({
 		height: 150,
 		backgroundColor: '#a9ffa1',
 		borderRadius: 20
+	},
+	Upload:{
+		margin: 15,
+		height: 250,
+		backgroundColor: '#3fffa1',
+		borderRadius: 25
 	}
 });
