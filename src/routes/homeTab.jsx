3e2b915f8cfeb65fc73fa5486fda27ff474e3fb7
Veerrohit Veeravdivel <veerrohitv08@gmail.com>
2021-07-02 08:57:42 -0700

    added third tab(reviewDetails)

--- src/routes/homeTab.jsx
+++ src/routes/homeTab.jsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import homeScreen from '../screens/homeScreen';
 import photoScreen from '../screens/photoScreen';
+import reviewDetails from '../screens/reviewDetails';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +14,7 @@ export default function Navigator() {
 			<Tab.Navigator>
 				<Tab.Screen name="Home" component={homeScreen} />
 				<Tab.Screen name="Upload" component={photoScreen} />
+				<Tab.Screen name="Details" component={reviewDetails} />
 			</Tab.Navigator>
 		</NavigationContainer>
 	);
