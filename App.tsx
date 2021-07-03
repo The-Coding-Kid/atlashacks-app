c0d9d6065cbf83cee3951a5fe13fde5708946cea
Veerrohit Veeravdivel <veerrohitv08@gmail.com>
2021-07-03 07:55:25 -0700

    removed a few stuff

--- App.tsx
+++ App.tsx
@@ -2,14 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './src/routes/homeTab';
-import { BlogProvider } from './src/context/context';
 
 export default function App() {
 	return (
-		<BlogProvider>
+		<>
 			<StatusBar style="auto" />
 			<Navigator />
-		</BlogProvider>
+		</>
 	);
 }
 
